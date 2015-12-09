d7a03f75a7011aec2d0fa00a3dba331933a0a716
Skyrda Dima <skyrda_dima@yahoo.com>
2015-12-09 12:47:48 +0200

    ADD description for pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def title(page_title)
 		content_for(:title) { page_title }
 	end
+
+ 	def meta_description(desc)
+      		content_for :meta_description, desc
+    end
 end
